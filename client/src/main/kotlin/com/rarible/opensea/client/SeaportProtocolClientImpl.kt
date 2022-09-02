b40b690d7d1acb59ffafe3b4b6b1c216b024ae51
Andrey Atapin <1181403+atapin@users.noreply.github.com>
2022-09-02 15:58:09 +0500

    feat: fetch listings by item (#3)

--- client/src/main/kotlin/com/rarible/opensea/client/SeaportProtocolClientImpl.kt
+++ client/src/main/kotlin/com/rarible/opensea/client/SeaportProtocolClientImpl.kt
@@ -4,6 +4,8 @@ import com.rarible.opensea.client.agent.UserAgentProvider
 import com.rarible.opensea.client.model.OpenSeaResult
 import com.rarible.opensea.client.model.v2.OrdersRequest
 import com.rarible.opensea.client.model.v2.SeaportOrders
+import scalether.domain.Address
+import java.math.BigInteger
 import java.net.URI
 
 class SeaportProtocolClientImpl(
@@ -20,6 +22,12 @@ class SeaportProtocolClientImpl(
             path("/v2/orders/${network.value}/seaport/listings")
             request.limit?.let { queryParam("limit", it) }
             request.cursor?.let { queryParam("cursor", it) }
+            request.address?.let { queryParam("asset_contract_address", it.toString()) }
+            request.tokenIds?.let {
+                it.forEach { tokenId ->
+                    queryParam("token_ids", tokenId.toString())
+                }
+            }
             build()
         }
         return getOpenSeaResult(uri)
--- client/src/main/kotlin/com/rarible/opensea/client/model/v2/OrdersRequest.kt
+++ client/src/main/kotlin/com/rarible/opensea/client/model/v2/OrdersRequest.kt
@@ -1,8 +1,15 @@
 package com.rarible.opensea.client.model.v2
 
+import scalether.domain.Address
+import java.math.BigInteger
+
 data class OrdersRequest(
     // Number of orders to retrieve (capped at 50).
     val limit: Int?,
     // A cursor to be supplied as a query param to retrieve the next page
-    val cursor: String?
+    val cursor: String?,
+
+    val token: Address? = null,
+
+    val tokenIds: List<BigInteger>? = null
 )
