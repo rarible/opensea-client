95075e0a6e510524cf48fc56cf333845517f1778
Dmitry Saraykin <d.saraykin@rarible.com>
2024-01-23 14:26:13 +0300

    fix(PT-4660): handle os order invalidation

--- subscriber/src/main/kotlin/com/rarible/opensea/subscriber/model/OpenseaEventType.kt
+++ subscriber/src/main/kotlin/com/rarible/opensea/subscriber/model/OpenseaEventType.kt
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonValue
 
 enum class OpenseaEventType(@get:JsonValue val value: String) {
-    ITEM_CANCELLED("item_cancelled");
+    ITEM_CANCELLED("item_cancelled"),
+    ORDER_INVALIDATED("order_invalidated"),
+    ORDER_REVALIDATED("order_revalidated");
 
     companion object {
         @JsonCreator
